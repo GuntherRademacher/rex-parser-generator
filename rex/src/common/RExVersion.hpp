fb44909d5f91cdfa510309bc49af54154dc6cb0c
Gunther Rademacher <grd@gmx.net>
2024-12-13 21:00:38 +0100

    set snapshot version

--- rex/src/common/RExVersion.hpp
+++ rex/src/common/RExVersion.hpp
@@ -8,8 +8,8 @@
 #ifndef REXVERSION_HPP
 #define REXVERSION_HPP
 #define REX_VMAJOR   "6"
-#define REX_VMINOR   "0"
-#define REX_DAY      29
-#define REX_MONTH    "November" // not abbreviated
+#define REX_VMINOR   "1-SNAPSHOT"
+#define REX_DAY      12
+#define REX_MONTH    "December"
 #define REX_YEAR     2024
 #endif
