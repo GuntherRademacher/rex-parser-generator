094ceae688c6752eb569acc06c3730a31f30de31
Gunther Rademacher <grd@gmx.net>
2025-01-30 19:39:28 +0100

    add parameter for passing folder

--- rex/scripts/fragments/regenerate.bat
+++ rex/scripts/fragments/regenerate.bat
@@ -4,9 +4,16 @@
 
 setlocal
 
-cd %~dp0..\..\src
+if not exist "%1" (
+  echo Usage: %~n0 FOLDER
+  echo/
+  echo Regenerates REx-generated source files in FOLDER and its subdirectories.
+  exit/b 1
+)
 
-for /f "delims=" %%A in ('findstr /m /s /c:"// This file was generated " *') do (
+cd %1
+
+for /f "delims=" %%A in ('findstr /m /s /c:" This file was generated on " *') do (
   echo %%~nxA 
   pushd %%~dpA 
   rex -remake %%~nxA 
